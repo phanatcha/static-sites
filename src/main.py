9b7464069bc9f46681510a653de9f1555e9f19fb
phanatcha <phanatcha19@gmail.com>
2025-06-28 23:53:11 +0700

    deleted:    docs/blog/glorfindel/index.html 	deleted:    docs/blog/majesty/index.html 	deleted:    docs/blog/tom/index.html 	deleted:    docs/contact/index.html 	deleted:    docs/index.html 	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,29 +1,26 @@
 import os
 import shutil
-import sys
+
 from copystatic import copy_files_recursive
 from generate_page import generate_pages_recursive
 
+
 dir_path_static = "./static"
-dir_path_public = "./docs"  # Changed from public to docs
-template_path = "./template.html"
+dir_path_public = "./public"
 dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    # Get basepath from CLI args or default to /
-    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
-    
-    print("Deleting docs directory...")
+    print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-    print("Copying static files to docs directory...")
+    print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    print("Generating pages from markdown files...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
-    print("Build complete!")
 
-if __name__ == "__main__":
-    main()
+main()
